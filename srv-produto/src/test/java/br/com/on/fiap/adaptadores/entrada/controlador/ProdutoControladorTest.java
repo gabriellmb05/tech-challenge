42aa93e303219a4a430fd687c27faef3f654a561
gabriellmb05 <gabriel.luiz.mb@gmail.com>
2024-12-28 16:44:24 -0300

    test: Ajustando testes que quebraram referente ao ProdutoControladorTest

--- srv-produto/src/test/java/br/com/on/fiap/adaptadores/entrada/controlador/ProdutoControladorTest.java
+++ srv-produto/src/test/java/br/com/on/fiap/adaptadores/entrada/controlador/ProdutoControladorTest.java
@@ -4,7 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import br.com.on.fiap.adaptadores.entrada.controlador.dto.ProdutoDTO;
+import br.com.on.fiap.adaptadores.entrada.controlador.dto.ProdutoRespostaDTO;
+import br.com.on.fiap.adaptadores.entrada.controlador.dto.ProdutoSolicitacaoDTO;
 import br.com.on.fiap.adaptadores.entrada.controlador.mapeador.ProdutoEntradaMapeador;
 import br.com.on.fiap.hexagono.dominio.Produto;
 import br.com.on.fiap.hexagono.portas.entrada.AlteraProdutoPortaEntrada;
@@ -16,6 +17,7 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -38,26 +40,33 @@ class ProdutoControladorTest {
 
   @InjectMocks private ProdutoControlador produtoControlador;
 
-  static Stream<ProdutoDTO> produtoDTOProvider() {
+  static Stream<Arguments> produtoDTOProvider() {
     return Stream.of(
-        new ProdutoDTO(1L, "Produto 1", "Descricao 1", BigDecimal.valueOf(10.0)),
-        new ProdutoDTO(2L, "Produto 2", "Descricao 2", BigDecimal.valueOf(20.0)),
-        new ProdutoDTO(3L, "Produto 3", "Descricao 3", BigDecimal.valueOf(30.0)));
+        Arguments.of(
+            new ProdutoRespostaDTO(1L, "Produto 1", "Descricao 1", BigDecimal.valueOf(10.0)),
+            new ProdutoSolicitacaoDTO("Produto 1", "Descricao 1", BigDecimal.valueOf(10.0))),
+        Arguments.of(
+            new ProdutoRespostaDTO(2L, "Produto 2", "Descricao 2", BigDecimal.valueOf(20.0)),
+            new ProdutoSolicitacaoDTO("Produto 2", "Descricao 2", BigDecimal.valueOf(20.0))),
+        Arguments.of(
+            new ProdutoRespostaDTO(3L, "Produto 3", "Descricao 3", BigDecimal.valueOf(30.0)),
+            new ProdutoSolicitacaoDTO("Produto 3", "Descricao 3", BigDecimal.valueOf(30.0))));
   }
 
   @ParameterizedTest
   @MethodSource("produtoDTOProvider")
   @DisplayName("Dado um produto existente, quando buscar o produto, então ele deve ser retornado")
-  void dadoProdutoExistente_quandoBuscarProduto_entaoDeveSerRetornado(ProdutoDTO produtoDTO) {
-    Long id = produtoDTO.id();
+  void dadoProdutoExistente_quandoBuscarProduto_entaoDeveSerRetornado(
+      ProdutoRespostaDTO produtoRespostaDTO) {
+    Long id = produtoRespostaDTO.id();
     Produto produto = new Produto();
     when(buscaProdutoPorIdPortaEntrada.buscar(id)).thenReturn(produto);
-    when(produtoEntradaMapeador.paraProdutoDTO(produto)).thenReturn(produtoDTO);
+    when(produtoEntradaMapeador.paraProdutoDTO(produto)).thenReturn(produtoRespostaDTO);
 
-    ResponseEntity<ProdutoDTO> response = produtoControlador.buscaProdutoPorId(id);
+    ResponseEntity<ProdutoRespostaDTO> response = produtoControlador.buscaProdutoPorId(id);
 
     assertEquals(HttpStatus.OK, response.getStatusCode());
-    assertEquals(produtoDTO, response.getBody());
+    assertEquals(produtoRespostaDTO, response.getBody());
     verify(buscaProdutoPorIdPortaEntrada).buscar(id);
     verify(produtoEntradaMapeador).paraProdutoDTO(produto);
   }
@@ -65,21 +74,20 @@ class ProdutoControladorTest {
   @ParameterizedTest
   @MethodSource("produtoDTOProvider")
   @DisplayName("Dado um produto novo, quando inserir o produto, então ele deve ser salvo")
-  void dadoProdutoNovo_quandoInserirProduto_entaoDeveSerSalvo(ProdutoDTO produtoDTO) {
+  void dadoProdutoNovo_quandoInserirProduto_entaoDeveSerSalvo(
+      ProdutoRespostaDTO produtoRespostaDTO, ProdutoSolicitacaoDTO produtoSolicitacaoDTO) {
     Produto produto = new Produto();
     Produto produtoPersistido = new Produto();
-    ProdutoDTO produtoPersistidoDTO =
-        new ProdutoDTO(
-            produtoDTO.id(), produtoDTO.nome(), produtoDTO.categoria(), produtoDTO.preco());
-    when(produtoEntradaMapeador.paraProduto(produtoDTO)).thenReturn(produto);
+    when(produtoEntradaMapeador.paraProduto(produtoSolicitacaoDTO)).thenReturn(produto);
     when(insereProdutoPortaEntrada.inserir(produto)).thenReturn(produtoPersistido);
-    when(produtoEntradaMapeador.paraProdutoDTO(produtoPersistido)).thenReturn(produtoPersistidoDTO);
+    when(produtoEntradaMapeador.paraProdutoDTO(produtoPersistido)).thenReturn(produtoRespostaDTO);
 
-    ResponseEntity<ProdutoDTO> response = produtoControlador.insereProduto(produtoDTO);
+    ResponseEntity<ProdutoRespostaDTO> response =
+        produtoControlador.insereProduto(produtoSolicitacaoDTO);
 
     assertEquals(HttpStatus.CREATED, response.getStatusCode());
-    assertEquals(produtoPersistidoDTO, response.getBody());
-    verify(produtoEntradaMapeador).paraProduto(produtoDTO);
+    assertEquals(produtoRespostaDTO, response.getBody());
+    verify(produtoEntradaMapeador).paraProduto(produtoSolicitacaoDTO);
     verify(insereProdutoPortaEntrada).inserir(produto);
     verify(produtoEntradaMapeador).paraProdutoDTO(produtoPersistido);
   }
@@ -87,22 +95,22 @@ class ProdutoControladorTest {
   @ParameterizedTest
   @MethodSource("produtoDTOProvider")
   @DisplayName("Dado um produto existente, quando alterar o produto, então ele deve ser atualizado")
-  void dadoProdutoExistente_quandoAlterarProduto_entaoDeveSerAtualizado(ProdutoDTO produtoDTO) {
-    Long id = produtoDTO.id();
+  void dadoProdutoExistente_quandoAlterarProduto_entaoDeveSerAtualizado(
+      ProdutoRespostaDTO produtoRespotaDTO, ProdutoSolicitacaoDTO produtoSolicitacaoDTO) {
+    Long id = produtoRespotaDTO.id();
     Produto produto = new Produto();
     Produto produtoPersistido = new Produto();
-    ProdutoDTO produtoPersistidoDTO =
-        new ProdutoDTO(
-            produtoDTO.id(), produtoDTO.nome(), produtoDTO.categoria(), produtoDTO.preco());
-    when(produtoEntradaMapeador.paraProduto(produtoDTO)).thenReturn(produto);
-    when(alteraProdutoPortaEntrada.alterar(id, produto)).thenReturn(produtoPersistido);
-    when(produtoEntradaMapeador.paraProdutoDTO(produtoPersistido)).thenReturn(produtoPersistidoDTO);
 
-    ResponseEntity<ProdutoDTO> response = produtoControlador.alteraProduto(id, produtoDTO);
+    when(produtoEntradaMapeador.paraProduto(produtoSolicitacaoDTO)).thenReturn(produto);
+    when(alteraProdutoPortaEntrada.alterar(id, produto)).thenReturn(produtoPersistido);
+    when(produtoEntradaMapeador.paraProdutoDTO(produtoPersistido)).thenReturn(produtoRespotaDTO);
+
+    ResponseEntity<ProdutoRespostaDTO> response =
+        produtoControlador.alteraProduto(id, produtoSolicitacaoDTO);
 
     assertEquals(HttpStatus.OK, response.getStatusCode());
-    assertEquals(produtoPersistidoDTO, response.getBody());
-    verify(produtoEntradaMapeador).paraProduto(produtoDTO);
+    assertEquals(produtoRespotaDTO, response.getBody());
+    verify(produtoEntradaMapeador).paraProduto(produtoSolicitacaoDTO);
     verify(alteraProdutoPortaEntrada).alterar(id, produto);
     verify(produtoEntradaMapeador).paraProdutoDTO(produtoPersistido);
   }
@@ -110,10 +118,11 @@ class ProdutoControladorTest {
   @ParameterizedTest
   @MethodSource("produtoDTOProvider")
   @DisplayName("Dado um produto existente, quando deletar o produto, então ele deve ser removido")
-  void dadoProdutoExistente_quandoDeletarProduto_entaoDeveSerRemovido(ProdutoDTO produtoDTO) {
-    Long id = produtoDTO.id();
+  void dadoProdutoExistente_quandoDeletarProduto_entaoDeveSerRemovido(
+      ProdutoRespostaDTO produtoRespostaDTO) {
+    Long id = produtoRespostaDTO.id();
 
-    ResponseEntity<ProdutoDTO> response = produtoControlador.deletaProduto(id);
+    ResponseEntity<Void> response = produtoControlador.deletaProduto(id);
 
     assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     verify(deletaProdutoPortaEntrada).deleta(id);
