4d74fd03e257fa42693c5092c1bd72476c8a07b6
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2025-01-12 23:01:18 -0300

    feat: formatando classes

--- srv-cliente/src/test/java/br/com/on/fiap/adaptadores/entrada/controlador/ClienteControladorIntegracaoTest.java
+++ srv-cliente/src/test/java/br/com/on/fiap/adaptadores/entrada/controlador/ClienteControladorIntegracaoTest.java
@@ -46,7 +46,8 @@ public class ClienteControladorIntegracaoTest {
 	@DisplayName("Dado um cliente novo, quando nao informar o cpf do cliente, então deve retornar erro de validação 'O atributo cpf é obrigatório'")
 	void dadoClienteNovo_quandoInserirClienteSemInformarCpf_entaoDeveRetornarErroDeValidacao() throws Exception {
 		mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComCpf(null)))).andExpect(status().isBadRequest())
+				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComCpf(null))))
+				.andExpect(status().isBadRequest())
 				.andExpect(jsonPath("$.errors[0]").value("O atributo cpf é obrigatório")).andReturn();
 	}
 
@@ -55,7 +56,8 @@ public class ClienteControladorIntegracaoTest {
 	@DisplayName("Dado um cliente novo, quando nao informar o email do cliente, então deve retornar erro de validação 'O atributo email é obrigatório'")
 	void dadoClienteNovo_quandoInserirClienteSemInformarEmail_entaoDeveRetornarErroDeValidacao() throws Exception {
 		mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComEmail(null)))).andExpect(status().isBadRequest())
+				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComEmail(null))))
+				.andExpect(status().isBadRequest())
 				.andExpect(jsonPath("$.errors[0]").value("O atributo email é obrigatório")).andReturn();
 	}
 
@@ -63,8 +65,9 @@ public class ClienteControladorIntegracaoTest {
 	@Order(4)
 	@DisplayName("Dado um cliente novo, quando inserir o cliente com CPF inválido, então deve retornar erro de validação 'O CPF informado é inválido'")
 	void dadoClienteNovo_quandoInserirClienteComCpfInvalido_entaoDeveRetornarErroDeValidacao() throws Exception {
-        mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComCpf("12312312312")))).andExpect(status().isBadRequest())
+		mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComCpf("12312312312"))))
+				.andExpect(status().isBadRequest())
 				.andExpect(jsonPath("$.errors[0]").value("O CPF informado é inválido")).andReturn();
 	}
 
@@ -72,8 +75,9 @@ public class ClienteControladorIntegracaoTest {
 	@Order(5)
 	@DisplayName("Dado um cliente novo, quando inserir o cliente com E-mail inválido, então deve retornar erro de validação 'O E-mail informado é inválido'")
 	void dadoClienteNovo_quandoInserirClienteComEmailInvalido_entaoDeveRetornarErroDeValidacao() throws Exception {
-        mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComEmail("teste")))).andExpect(status().isBadRequest())
+		mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComEmail("teste"))))
+				.andExpect(status().isBadRequest())
 				.andExpect(jsonPath("$.errors[0]").value("O E-mail informado é inválido")).andReturn();
 	}
 
@@ -81,9 +85,10 @@ public class ClienteControladorIntegracaoTest {
 	@Order(6)
 	@DisplayName("Dado um cliente novo, quando inserir o cliente, então ele deve ser salvo")
 	void dadoClienteNovo_quandoInserirCliente_entaoDeveSerSalvo() throws Exception {
-        mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComCpfEmail("10903153092", "teste@gmail.com")))).andExpect(status().isCreated())
-				.andReturn();
+		mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(
+						DataPoolClienteSolicitacaoDTO.gerarComCpfEmail("10903153092", "teste@gmail.com"))))
+				.andExpect(status().isCreated()).andReturn();
 	}
 
 	@Test
@@ -107,8 +112,9 @@ public class ClienteControladorIntegracaoTest {
 	@Order(9)
 	@DisplayName("Dado um cliente novo, quando inserir o cliente com cpf já existente, então deve retornar erro de validação 'O CPF de número {valor} já foi utilizado'")
 	void dadoClienteNovo_quandoInserirClienteComCpfJaCadastrado_entaoDeveRetornarErroDeValidacao() throws Exception {
-        mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComCpf("10903153092")))).andExpect(status().isBadRequest())
+		mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComCpf("10903153092"))))
+				.andExpect(status().isBadRequest())
 				.andExpect(jsonPath("$.errors[0]").value("O CPF de número 10903153092 já foi utilizado")).andReturn();
 	}
 
@@ -116,9 +122,9 @@ public class ClienteControladorIntegracaoTest {
 	@Order(10)
 	@DisplayName("Dado um cliente novo, quando inserir o cliente com E-mail já existente, então deve retornar erro de validação 'O E-mail {valor} já foi utilizado'")
 	void dadoClienteNovo_quandoInserirClienteComEmailJaCadastrado_entaoDeveRetornarErroDeValidacao() throws Exception {
-		mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON)
-				.content(objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComEmail("teste@gmail.com")))).andExpect(status().isBadRequest())
+		mockMvc.perform(post("/clientes").contentType(MediaType.APPLICATION_JSON).content(
+				objectMapper.writeValueAsString(DataPoolClienteSolicitacaoDTO.gerarComEmail("teste@gmail.com"))))
+				.andExpect(status().isBadRequest())
 				.andExpect(jsonPath("$.errors[0]").value("O E-mail teste@gmail.com já foi utilizado")).andReturn();
 	}
-
 }
