419b3c90af46da06cdede3b53fd8210ad94cc485
Max Barboza <maxsuelstorch@gmail.com>
2025-01-12 23:00:09 -0300

    Refatorando testes para utilizar datapool

--- srv-cliente/src/test/java/br/com/on/fiap/datapool/DataPoolClienteSolicitacaoDTO.java
+++ srv-cliente/src/test/java/br/com/on/fiap/datapool/DataPoolClienteSolicitacaoDTO.java
@@ -1,27 +1,17 @@
 package br.com.on.fiap.datapool;
 
 import br.com.on.fiap.adaptadores.entrada.controlador.dto.ClienteSolicitacaoDTO;
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.IntStream;
 import net.datafaker.Faker;
+
+import java.time.LocalDate;
 
 public class DataPoolClienteSolicitacaoDTO {
 
-	private static Faker faker;
-
-	private DataPoolClienteSolicitacaoDTO() {
-		faker = new Faker();
-	}
+	private static final Faker faker = new Faker();
 
 	private static ClienteSolicitacaoDTO construirCliente(String nome, String cpf, String email,
 			LocalDate dataNascimento) {
 		return ClienteSolicitacaoDTO.builder().nome(nome).cpf(cpf).email(email).dataNascimento(dataNascimento).build();
-	}
-
-	private static ClienteSolicitacaoDTO criarClienteComDadosFakes() {
-		return construirCliente(faker.name().fullName(), faker.cpf().valid(), faker.internet().emailAddress(),
-				LocalDate.now());
 	}
 
 	public static ClienteSolicitacaoDTO gerarComCpf(String cpf) {
@@ -36,11 +26,7 @@ public class DataPoolClienteSolicitacaoDTO {
 		return construirCliente(faker.name().fullName(), faker.cpf().valid(), email, LocalDate.now());
 	}
 
-	public static ClienteSolicitacaoDTO gerarClienteComDadosFakes() {
-		return criarClienteComDadosFakes();
-	}
-
-	public static List<ClienteSolicitacaoDTO> gerarClientesFakes(int quantidade) {
-		return IntStream.range(0, quantidade).mapToObj(i -> criarClienteComDadosFakes()).toList();
+	public static ClienteSolicitacaoDTO gerarComCpfEmail(String cpf, String email) {
+		return construirCliente(faker.name().fullName(), cpf, email, LocalDate.now());
 	}
 }
