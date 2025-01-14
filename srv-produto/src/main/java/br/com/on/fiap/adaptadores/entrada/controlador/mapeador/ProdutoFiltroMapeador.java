9e43742c173da8477378cfd6a7fbe2d8897eaf78
gabriellmb05 <gabriel.luiz.mb@gmail.com>
2025-01-14 13:31:05 -0300

    Test: Adicionando teste para a instanciação do bean da classe ListarProdutoPortaEntrada.

--- srv-produto/src/main/java/br/com/on/fiap/adaptadores/entrada/controlador/mapeador/ProdutoFiltroMapeador.java
+++ srv-produto/src/main/java/br/com/on/fiap/adaptadores/entrada/controlador/mapeador/ProdutoFiltroMapeador.java
@@ -1,13 +1,20 @@
 package br.com.on.fiap.adaptadores.entrada.controlador.mapeador;
 
 import br.com.on.fiap.adaptadores.entrada.controlador.dto.ProdutoFiltroDTO;
+import br.com.on.fiap.hexagono.dominio.Categoria;
 import br.com.on.fiap.hexagono.dominio.ProdutoFiltro;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface ProdutoFiltroMapeador {
 
 	@Mapping(target = "categoria", qualifiedByName = "getCategoria")
 	ProdutoFiltro paraProdutoFiltro(ProdutoFiltroDTO produtoFiltroDTO);
+
+    @Named("getCategoria")
+    default Categoria getCategoria(String categoria) {
+        return Categoria.buscaCategoria(categoria.toUpperCase());
+    }
 }
