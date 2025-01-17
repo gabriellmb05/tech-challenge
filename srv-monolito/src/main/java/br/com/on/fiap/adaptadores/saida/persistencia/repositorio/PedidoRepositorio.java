1a989d1fd90a44308efa45d65b5db8dfdb85adde
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2025-01-17 16:59:44 -0300

    feat: correcao filtro de data

--- srv-monolito/src/main/java/br/com/on/fiap/adaptadores/saida/persistencia/repositorio/PedidoRepositorio.java
+++ srv-monolito/src/main/java/br/com/on/fiap/adaptadores/saida/persistencia/repositorio/PedidoRepositorio.java
@@ -1,50 +1,17 @@
 package br.com.on.fiap.adaptadores.saida.persistencia.repositorio;
 
 import br.com.on.fiap.adaptadores.saida.persistencia.entidade.PedidoEntidade;
-import br.com.on.fiap.hexagono.dominio.PedidoFiltro;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface PedidoRepositorio extends JpaRepository<PedidoEntidade, Long> {
+public interface PedidoRepositorio
+		extends
+			JpaRepository<PedidoEntidade, Long>,
+			JpaSpecificationExecutor<PedidoEntidade> {
 
 	Optional<PedidoEntidade> findByProtocolo(String protocolo);
-
-	@Query("""
-			SELECT
-			    p
-			FROM
-			    PedidoEntidade p
-			JOIN p.cliente c
-			WHERE
-			    (
-			        :#{#filtro.situacao} IS NULL
-			            OR p.situacao = :#{#filtro.situacao}
-			    )
-			    AND (
-			        :#{#filtro.cpfCliente} IS NULL
-			            OR c.cpf = :#{#filtro.cpfCliente}
-			    )
-			    AND (
-			        :#{#filtro.dataInicio} IS NULL
-			            OR DATE_TRUNC(
-			                'day',
-			                p.dataHora
-			            ) >= :#{#filtro.dataInicio}
-			    )
-			    AND (
-			        :#{#filtro.dataFim} IS NULL
-			            OR DATE_TRUNC(
-			                'day',
-			                p.dataHora
-			            ) <= :#{#filtro.dataFim}
-			    )
-			""")
-	Page<PedidoEntidade> buscarPedidosPorFiltro(@Param(value = "filtro") PedidoFiltro filtro, Pageable pageable);
 }
--- srv-monolito/src/main/java/br/com/on/fiap/adaptadores/saida/servico/BuscaPedidosAdaptador.java
+++ srv-monolito/src/main/java/br/com/on/fiap/adaptadores/saida/servico/BuscaPedidosAdaptador.java
@@ -1,6 +1,7 @@
 package br.com.on.fiap.adaptadores.saida.servico;
 
 import br.com.on.fiap.adaptadores.saida.persistencia.mapeador.PedidoSaidaMapeador;
+import br.com.on.fiap.adaptadores.saida.persistencia.especificacao.PedidoEspecificacao;
 import br.com.on.fiap.adaptadores.saida.persistencia.repositorio.PedidoRepositorio;
 import br.com.on.fiap.hexagono.dominio.Pedido;
 import br.com.on.fiap.hexagono.dominio.PedidoFiltro;
@@ -24,6 +25,7 @@ public class BuscaPedidosAdaptador implements BuscaPedidosPortaSaida {
 	@Override
 	@Transactional(readOnly = true)
 	public Page<Pedido> listarComFiltros(PedidoFiltro filtro, Pageable page) {
-		return pedidoRepositorio.buscarPedidosPorFiltro(filtro, page).map(pedidoSaidaMapeador::paraPedido);
+		return pedidoRepositorio.findAll(PedidoEspecificacao.filtroPorDataInicioEDataFim(filtro), page)
+				.map(pedidoSaidaMapeador::paraPedido);
 	}
 }
