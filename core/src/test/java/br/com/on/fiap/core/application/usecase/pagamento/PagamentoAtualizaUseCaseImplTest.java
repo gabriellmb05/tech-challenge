75d4f2a5f92269714b498a026547ef577b7262ff
Tiago Boaventura Miranda <tgo.bm.dev@outlook.com>
2025-03-12 02:24:08 -0300

    melhoria na estrutura proposta

--- core/src/test/java/br/com/on/fiap/core/application/usecase/pagamento/PagamentoAtualizaUseCaseImplTest.java
+++ core/src/test/java/br/com/on/fiap/core/application/usecase/pagamento/PagamentoAtualizaUseCaseImplTest.java
@@ -3,18 +3,25 @@ package br.com.on.fiap.core.application.usecase.pagamento;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import br.com.on.fiap.core.application.exception.PagamentoJaRealizadoExcecao;
 import br.com.on.fiap.core.application.gateway.PagamentoGateway;
 import br.com.on.fiap.core.application.gateway.PagamentoIntegracaoGateway;
 import br.com.on.fiap.core.application.usecase.pagamento.impl.PagamentoAtualizaUseCaseImpl;
 import br.com.on.fiap.core.application.usecase.pedido.PedidoDetalhaUseCase;
 import br.com.on.fiap.core.domain.Pagamento;
+import br.com.on.fiap.core.domain.Pedido;
 import br.com.on.fiap.core.domain.SituacaoPagamento;
+import br.com.on.fiap.core.domain.TipoPagamento;
+import br.com.on.fiap.datapool.PagamentoDataPool;
 import br.com.on.fiap.datapool.PedidoDataPool;
-import org.junit.jupiter.api.BeforeEach;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class PagamentoAtualizaUseCaseImplTest {
 
     @Mock
@@ -32,52 +39,39 @@ class PagamentoAtualizaUseCaseImplTest {
     @InjectMocks
     private PagamentoAtualizaUseCaseImpl pagamentoAtualizaUseCase;
 
-    private Pagamento pagamento;
-
-    @BeforeEach
-    void setUp() {
-        MockitoAnnotations.openMocks(this);
-        pagamento = mock(Pagamento.class);
-    }
-
     @Test
     @DisplayName("Quando o pagamento for atualizado, então ele deve ser enviado e salvo corretamente")
     void quandoAtualizarPagamento_entaoPagamentoDeveSerEnviadoESalvo() {
-        // Preparando mocks
         String nrProtocolo = "12345";
-        when(pedidoDetalhaUseCase.detalhaPedido(nrProtocolo)).thenReturn(PedidoDataPool.criarPedidoExistente(1L));
+        Pedido pedido = PedidoDataPool.criarPedidoExistente(1L);
+        when(pedidoDetalhaUseCase.detalhaPedido(nrProtocolo)).thenReturn(pedido);
+        when(pagamentoGateway.salvaPagamento(any())).thenReturn(pedido.getPagamento());
 
-        // Configurando o pagamento mockado
-        when(pagamento.getStPagamento()).thenReturn(SituacaoPagamento.PENDENTE);
-
-        // Executa o método que será testado
         Pagamento resultado = pagamentoAtualizaUseCase.atualizaPagamento(nrProtocolo);
 
-        // Verificando as interações e assertivas
         verify(pedidoDetalhaUseCase).detalhaPedido(nrProtocolo);
-        verify(pagamentoValidaUseCase).validarPagamentoJaRealizado(pagamento, nrProtocolo);
-        verify(pagamentoIntegracaoGateway).integracaoEnviaPagamento(pagamento);
+        verify(pagamentoValidaUseCase).validarPagamentoJaRealizado(pedido.getPagamento(), nrProtocolo);
+        verify(pagamentoIntegracaoGateway).integracaoEnviaPagamento(pedido.getPagamento());
         verify(pagamentoGateway).salvaPagamento(resultado);
 
         assertNotNull(resultado);
-        assertEquals(
-                SituacaoPagamento.APROVADO,
-                resultado.getStPagamento()); // Verifica se a situação foi alterada para 'APROVADO'
-        assertNotNull(resultado.getDhPagamento()); // Verifica se a data de pagamento foi atribuída
+        assertEquals(SituacaoPagamento.APROVADO, resultado.getStPagamento());
+        assertNotNull(resultado.getDhPagamento());
     }
 
     @Test
     @DisplayName("Quando o pagamento já foi realizado, então deve lançar uma exceção")
     void quandoPagamentoJaRealizado_entaoDeveLancarExcecao() {
-        // Preparando mocks
         String nrProtocolo = "12345";
-        when(pedidoDetalhaUseCase.detalhaPedido(nrProtocolo)).thenReturn(PedidoDataPool.criarPedidoExistente(1L));
 
-        // Configurando o pagamento para já ter sido aprovado
-        when(pagamento.getStPagamento()).thenReturn(SituacaoPagamento.APROVADO);
+        Pagamento pagamento =
+                PagamentoDataPool.criarPagamentoComTipoESituacao(1L, TipoPagamento.PIX, SituacaoPagamento.APROVADO);
+        pagamento.setDhPagamento(LocalDateTime.now());
 
-        // Executa o método e verifica se a exceção é lançada
-        assertThrows(IllegalStateException.class, () -> pagamentoAtualizaUseCase.atualizaPagamento(nrProtocolo));
+        Pedido pedido = PedidoDataPool.criarPedidoExistente(1L);
+        pedido.setPagamento(pagamento);
+
+        assertThrows(PagamentoJaRealizadoExcecao.class, () -> pagamentoAtualizaUseCase.atualizaPagamento(nrProtocolo));
 
         verify(pedidoDetalhaUseCase).detalhaPedido(nrProtocolo);
         verifyNoInteractions(pagamentoValidaUseCase, pagamentoIntegracaoGateway, pagamentoGateway);
